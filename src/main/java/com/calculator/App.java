a3e68777e7b98853feaf7d3286a05fc0e2852041
Aaron Bittel <aaron@bittels.de>
2026-04-21 14:30:29 +0200

    fix: rename equation -> expression

--- src/main/java/com/calculator/App.java
+++ src/main/java/com/calculator/App.java
@@ -6,18 +6,18 @@ import java.util.stream.Collectors;
 public class App {
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.err.println("ERROR: no equation provided");
+            System.err.println("ERROR: no expression provided");
             System.exit(1);
         }
 
-        String equation = args[0];
+        String expression = args[0];
 
-        Lexer lexer = new Lexer(equation);
+        Lexer lexer = new Lexer(expression);
         try {
             List<Token> tokens = lexer.tokenize();
             Parser parser = new Parser(tokens);
             Expression expr = parser.parse();
-            System.out.println(equation);
+            System.out.println(expression);
             System.out.println(">>> " + expr.result());
         } catch (UnknownCharException e) {
             System.err.println("Error: " + e.getMessage());
